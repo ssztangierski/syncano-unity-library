3df765cfe3347ff1a21b2cc27ddd18647bcd30d6
Sebastian Sztangierski <sebastian.sztangierski@pixzell.pl>
2016-08-19 15:31:36 +0200

    [Problem/Solution] Merge post and get methods in one function

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,15 +6,14 @@ public class Test : MonoBehaviour {
 
 	IEnumerator Start()
 	{
-		Syncano s = Syncano.Instance;
-		s.Init("adfee89ec99b7022de16383185b299c29459e1fa", "unity-quiz-app");
+		Syncano s = Syncano.Instance.Init("adfee89ec99b7022de16383185b299c29459e1fa", "unity-quiz-app");
 
 		// CREATE NEW OBJECT
 		/*
 		Question q = new Question();
 		q.text = "new text";
 		q.answers = new string[] {"a", "b", "c", "d"};
-		Syncano.Instance.Please().Save(q, res);
+		Syncano.Instance.Please().Save(q, onSuccess, onFailure);
 		*/
 
 		// MODIFY TEXT
@@ -32,34 +31,25 @@ public class Test : MonoBehaviour {
 		// DELETE OBJECT
 		/*
 		Question q = new Question();
-		q.id = 163;
-		Syncano.Instance.Please().Delete(q, res);
+		q.id = 232;
+		Syncano.Instance.Please().Delete(q, onSuccess);
 		*/
 
 		// GET ONE QUESTION
-	//	yield return s.Please().Get(179, onSuccess);
+		//yield return s.Please().Get<Question>(227, onSuccess);
 
 	//	yield return s.Please().CallScriptEndpoint<Questions> ("d019a1036c7ec1348713de2770385b728f050ed1", "get_questions", null);
 
-		yield return s.Please().CallScriptEndpoint("6349c3ec1208c0be5ade53b154427d4eb5cb1628", "get_question_to_moderate", onSuccessScriptEndpoint);
-
-
-
+		//yield return s.Please().CallScriptEndpoint("6349c3ec1208c0be5ade53b154427d4eb5cb1628", "get_question_to_moderate", onSuccessScriptEndpoint);
 		yield return null;
 	}
 
-
-
 	private void onSuccessScriptEndpoint (ScriptEndpoint res) {
 
-		Question q = Question.FromJson(res.result.stdout);
+		Question q = JsonUtility.FromJson<Question>(res.result.stdout);// Question.FromJson(res.result.stdout);
 		Debug.Log(res.result.stdout);
 		Debug.Log(" q to moderate id= " + q.id + " ;length" + q.answers.Length);
-
-
 	}
-
-
 
 
 	/*
@@ -70,13 +60,14 @@ public class Test : MonoBehaviour {
 	}
 */
 
-
 	private void onSuccess (Response<Question> res)
 	{
-		
-		//Debug.Log(res.Data.text);
+		Debug.Log(res.Data.text);
 	}
 
-
+	private void onFailure (Response<Question> res)
+	{
+		Debug.Log(res.Data.text);
+	}
 
 }
